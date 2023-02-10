29a0b76f90961b9abca0da25f6521d5c5b7f8f91
noviciok <112391008+noviciok@users.noreply.github.com>
2023-02-10 11:16:40 +0200

    links

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('<slug:slug>', views.BlogView.as_view(), name='blog_view'),
-    path('', views.HomeView.as_view(), name='home_view')
+    path('about/', views.AboutView.as_view(), name='about_view'),
+    path('', views.PostList.as_view(), name= 'home')
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,5 +8,9 @@ class BlogView(generic.DetailView):
     model = Post
     template_name = 'blog.html'
 
-class HomeView(generic.TemplateView):
+class AboutView(generic.TemplateView):
+    template_name = 'about.html'
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-date_created')
     template_name = 'index.html'
