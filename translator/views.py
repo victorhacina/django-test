25977bfcbffbaef63e6a8f2792fb9af393737ea6
noviciok <112391008+noviciok@users.noreply.github.com>
2023-02-14 16:32:54 +0200

    div

--- translator/views.py
+++ translator/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def translator_view(request):
+    if request.method == 'POST':
+        original_text = request.POST['my_textarea']
+        output = original_text.upper()
+        return render(request, 'translator.html', {'output_text':output, 'original_text': original_text})
+    else:
+        return render(request, 'translator.html')
